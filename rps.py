f554a303f75ba931d9bf58ca4bd4a1b3f5b6fd1a
Elijah Yajaman <elijahyajaman23@gmail.com>
2022-09-06 13:48:47 +1200

    Migrated to PySimpleGUIQt

--- rps.py
+++ rps.py
@@ -14,7 +14,7 @@
 
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <https://www.gnu.org/licenses/>.
-import PySimpleGUI as sg
+import PySimpleGUIQt as sg
 import random as rnd
 import os , sys
 userChoice = ""
